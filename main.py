c63d10b4787134b2ee7e8617b2bbb5398def56d1
HaveSomeRubicon <97649192+HaveSomeRubicon@users.noreply.github.com>
2022-04-25 19:43:49 -0400

    Added ability to break up each line into individual pieces

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import time
 class Interpret():
     def __init__(self, path) -> None:
         self.path = path
 
     def interpret(self):
-        with open(self.path) as crpy_file:
-            print(crpy_file.readlines())
+        with open(self.path) as file:
+            file_lines = [line.replace('\n', '') for line in file.readlines()]
+        file_lines = [line.split(' ') for line in file_lines]
 
+        
+                    
 
 coompiler = Interpret('main.crpy')
 coompiler.interpret()
